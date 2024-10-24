523632b8e34c7a553a5b3a5e1df1ee8bf0aed296
high-ubuntu <highcloud100@inha.edu>
2024-10-24 21:59:31 +0900

    Add - error control ( send to client error ) - timer publishing logic
    we need some validation about these implemetation.
    - delete candidates
    - race condition

--- server/src/events/task.rs
+++ server/src/events/task.rs
@@ -2,6 +2,8 @@ use crate::data_model::msg::{ClientMsg, Msg, ServerMsg};
 use futures_util::stream::SplitSink;
 use futures_util::SinkExt;
 use raft::Commit;
+use tokio::sync::TryLockError;
+use tokio_tungstenite::tungstenite::client;
 use std::collections::HashMap;
 use std::sync::{Arc, Mutex};
 use tokio::net::TcpStream;
@@ -26,7 +28,7 @@ pub struct Writer {
 // - It preseves the stream that sended from the client_handler
 // - Receives committed messages from the Raft and sends them to the clients
 pub struct Publisher {
-    state_machine: Vec<Msg>,
+    state_machine:   Arc<tokio::sync::Mutex<Vec<Msg>>>,
 
     // < client's address, client's committed index >
     // shared with writer
@@ -34,6 +36,9 @@ pub struct Publisher {
 
     // < client's address, client stream >
     clients: Arc<tokio::sync::Mutex<HashMap<String, Stream>>>,
+
+    // lock
+    pub_lock : Arc<tokio::sync::Mutex<u8>>
 }
 
 impl Publisher {
@@ -43,9 +48,10 @@ impl Publisher {
         client_commit_idx: Arc<tokio::sync::Mutex<HashMap<String, u64>>>,
     ) -> Self {
         Publisher {
-            state_machine,
+            state_machine: Arc::new(tokio::sync::Mutex::new(state_machine)),
             client_commit_idx,
             clients: Arc::new(tokio::sync::Mutex::new(HashMap::new())),
+            pub_lock: Arc::new(tokio::sync::Mutex::new(0)),
         }
     }
 
@@ -71,54 +77,182 @@ impl Publisher {
         // - Need some error handling and disconnection handling
         let clients = self.clients.clone();
         let client_commit_idx = self.client_commit_idx.clone();
-        let mut state_machine = self.state_machine.clone();
+        let state_machine = self.state_machine.clone();
+        let pub_lock = self.pub_lock.clone();
         tokio::spawn(async move {
             while let Some(commit) = commit_rx.recv().await {
+                let lock = pub_lock.lock().await;
+                println!("got lock");
                 let raft_commit_idx = commit.get_index();
 
                 // [Warn] type error
-                if raft_commit_idx != state_machine.len() as u64 {
+                if raft_commit_idx != state_machine.lock().await.len() as u64 {
                     panic!(" split brained between raft and state machine");
                 }
                 let c_msg: Msg = bincode::deserialize(&commit.get_data()).unwrap();
-                state_machine.push(c_msg);
+                state_machine.lock().await.push(c_msg);
+                let mut delete_candidates = Vec::new();
 
-                let mut clients: tokio::sync::MutexGuard<
-                    '_,
-                    HashMap<String, SplitSink<WebSocketStream<TcpStream>, Message>>,
-                > = clients.lock().await;
+                {
+                    let mut clients_: tokio::sync::MutexGuard<
+                        '_,
+                        HashMap<String, SplitSink<WebSocketStream<TcpStream>, Message>>,
+                    > = clients.lock().await;
 
-                // publish
-                for (addr, client_stream) in clients.iter_mut() {
-                    let client_idx;
-                    {
-                        let client_commit_idx = client_commit_idx.lock().await;
-                        client_idx = client_commit_idx.get(addr).unwrap().clone();
+                    // publish
+                    for (addr, client_stream) in clients_.iter_mut() {
+                        let client_idx;
+                        {
+                            let client_commit_idx = client_commit_idx.lock().await;
+                            client_idx = client_commit_idx.get(addr).unwrap_or(&0).clone();
+                        }
+
+                        // build server msg
+                        let mut server_msgs = Vec::new();
+
+                        for i in client_idx..=raft_commit_idx {
+                            let temp = ServerMsg::new(i, state_machine.lock().await[i as usize].clone());
+                            server_msgs.push(temp);
+                        }
+
+                        println!(
+                            "tick Sending to {:?} cli idx: ({:?}): msg len: {:?} raft idx: {:?}",
+                            addr,
+                            client_idx,
+                            server_msgs.len(),
+                            raft_commit_idx
+                        );
+
+                        let res = client_stream
+                            .send(Message::Text(serde_json::to_string(&server_msgs).unwrap()))
+                            .await;
+
+                        match res {
+                            Ok(_) => {
+                                
+                            }
+                            Err(_) => {
+                                println!("Failed to send to {:?}", addr);
+                                delete_candidates.push(addr.clone());
+                            }
+                        }
                     }
+                }
 
-                    // build server msg
-                    let mut server_msgs = Vec::new();
+                {
+                    let mut clients_: tokio::sync::MutexGuard<
+                        '_,
+                        HashMap<String, SplitSink<WebSocketStream<TcpStream>, Message>>,
+                    > = clients.lock().await;
 
-                    for i in client_idx..=raft_commit_idx {
-                        let temp = ServerMsg::new(i, state_machine[i as usize].clone());
-                        server_msgs.push(temp);
+                    for addr in delete_candidates.iter() {
+                        println!("connection closed {:?}", addr);
+                        client_commit_idx.lock().await.remove(addr);
+                        clients_.remove(addr);
                     }
+                }
 
-                    println!(
-                        "Sending to {:?} cli idx: ({:?}): msg len: {:?} raft idx: {:?}",
-                        addr,
-                        client_idx,
-                        server_msgs.len(),
-                        raft_commit_idx
-                    );
+                drop(lock);
+            }
+        });
 
-                    client_stream
-                        .send(Message::Text(serde_json::to_string(&server_msgs).unwrap()))
-                        .await
-                        .unwrap();
+        let clients = self.clients.clone();
+        let client_commit_idx = self.client_commit_idx.clone();
+        let state_machine = self.state_machine.clone();
+        let pub_lock = self.pub_lock.clone();
+        tokio::spawn(async move {
+            let mut interval = time::interval(Duration::from_secs(5));
+            loop {
+                interval.tick().await;
+
+                let lock = pub_lock.try_lock();
+                match lock {
+                    Ok(_) => {
+                        let raft_commit_idx;
+                        if state_machine.lock().await.len() == 0 {
+                            drop(lock);
+                            continue;
+                        }
+                        else {
+                            raft_commit_idx = state_machine.lock().await.len() as u64 - 1;
+                        }
+
+                        let mut delete_candidates = Vec::new();
+
+                        {
+                            let mut clients_: tokio::sync::MutexGuard<
+                                '_,
+                                HashMap<String, SplitSink<WebSocketStream<TcpStream>, Message>>,
+                            > = clients.lock().await;
+
+                           
+
+                            // publish
+                            for (addr, client_stream) in clients_.iter_mut() {
+                                let client_idx;
+                                {
+                                    let client_commit_idx = client_commit_idx.lock().await;
+                                    client_idx = client_commit_idx.get(addr).unwrap_or(&0).clone();
+                                }
+
+                                // build server msg
+                                let mut server_msgs = Vec::new();
+
+                                for i in client_idx..=raft_commit_idx {
+                                    let temp = ServerMsg::new(i, state_machine.lock().await[i as usize].clone());
+                                    server_msgs.push(temp);
+                                }
+
+                                println!(
+                                    "tick Sending to {:?} cli idx: ({:?}): msg len: {:?} raft idx: {:?}",
+                                    addr,
+                                    client_idx,
+                                    server_msgs.len(),
+                                    raft_commit_idx
+                                );
+
+                                let res = client_stream
+                                    .send(Message::Text(serde_json::to_string(&server_msgs).unwrap()))
+                                    .await;
+
+                                match res {
+                                    Ok(_) => {
+                                        
+                                    }
+                                    Err(_) => {
+                                        println!("Failed to send to {:?}", addr);
+                                        delete_candidates.push(addr.clone());
+                                    }
+                                }
+                            }
+                        }
+
+                        {
+                            let mut clients_: tokio::sync::MutexGuard<
+                                '_,
+                                HashMap<String, SplitSink<WebSocketStream<TcpStream>, Message>>,
+                            > = clients.lock().await;
+
+                            for addr in delete_candidates.iter() {
+                                println!("connection closed {:?}", addr);
+                                client_commit_idx.lock().await.remove(addr);
+                                clients_.remove(addr);
+                            }
+
+                            println!("now clients {:?} {:?}", 
+                                client_commit_idx.lock().await.len(),
+                                clients_.len()
+                            )
+                        }
+                        drop(lock);
+                    }
+                    Err(_) => {
+                        continue;
+                    }
                 }
             }
         });
+
     }
 }
 
