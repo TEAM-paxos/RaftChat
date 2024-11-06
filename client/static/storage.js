60ef8032ddb723b42e2a521bec65e7bd16ad57e2
high-ubuntu <highcloud100@inha.edu>
2024-11-06 23:00:41 +0900

    Add localstorage in client. - now share the state between tabs.

---
+++ client/static/storage.js
@@ -0,0 +1,53 @@
+export class Storage {
+
+    setServerVersion(version) {
+        localStorage.setItem('serverVersion', version);
+    }
+
+    getServerVersion() {
+        return localStorage.getItem('serverVersion');
+    }
+
+    setTimeStamp(timeStamp) {
+        return localStorage.setItem('timeStamp', timeStamp);
+    }
+
+    getTimeStamp(timeStamp) {
+        return localStorage.getItem('timeStamp', timeStamp);
+    }
+
+    setIdUid(id, uid) {
+        localStorage.setItem('id', id);
+        localStorage.setItem('uid', uid);
+    }
+
+    getUid() {
+        return localStorage.getItem('uid');
+    }
+
+    getId() {
+        return localStorage.getItem('id');
+    }
+
+    setLatestIdx(idx) {
+        localStorage.setItem('latestIdx', idx);
+    }
+
+    getLatestIdx() {
+        return localStorage.getItem('latestIdx');
+    }
+
+    // idx, msg : json
+    saveMessage(idx, msg) {
+        localStorage.setItem(idx, JSON.stringify(msg));
+    }
+
+    // idx -> json
+    getMessage(idx) {
+        return JSON.parse(localStorage.getItem(idx));
+    }
+
+    clear() {
+        localStorage.clear();
+    }
+}
