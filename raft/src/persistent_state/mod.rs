eda1a87b2e6f6d9d9395f16ad83f0bb52aa274c0
RememberIOm <rememberiom@icloud.com>
2024-11-23 17:41:37 +0900

    Add PersistentStateElement struct & Refactor impl of PersistentState

--- raft/src/persistent_state/mod.rs
+++ raft/src/persistent_state/mod.rs
@@ -5,48 +5,52 @@ use serde::{Deserialize, Serialize};
 use serde_json;
 use std::fs;
 use std::io::{Read, Write};
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
 #[derive(Serialize, Deserialize)]
-pub struct PersistentState {
-    // These data must be stored on persistent storage
+
+pub struct PersistentStateElement {
     current_term: u64,
     voted_for: Option<&'static str>,
 }
 
+pub struct PersistentState {
+    element: PersistentStateElement,
+    path: PathBuf,
+    backup_path: PathBuf,
+}
+
 impl PersistentState {
-    pub fn new(path: &Path) -> PersistentState {
-        // TODO : initialize with data from path
+    pub fn new(path: &Path, backup_path: &Path) -> PersistentState {
         PersistentState {
-            current_term: 0,
-            voted_for: None,
+            element: PersistentStateElement {
+                current_term: 0,
+                voted_for: None,
+            },
+            path: path.to_path_buf(),
+            backup_path: backup_path.to_path_buf(),
         }
     }
 
     pub fn current_term(&self) -> u64 {
-        self.current_term
+        self.element.current_term
     }
 
     pub fn voted_for(&self) -> Option<&'static str> {
-        self.voted_for
+        self.element.voted_for
     }
 
     fn save(&self, path: &Path) {
-        let serialized = serde_json::to_vec(self)
-                                              .expect("Failed to serialize persistent state");
-        let mut file = AtomicWriteFile::options()
-                                                        .open(path)
-                                                        .expect("Failed to open file");
-        file.write_all(&serialized)
-            .expect("Failed to write to file");
-        file.commit()
-            .expect("Failed to commit file");
+        let serialized =
+            serde_json::to_vec(&self.element).expect("Failed to serialize persistent state");
+
+        // TODO : save serialized data to path
     }
 
     // Dummy implementation
     pub fn start_election(&mut self, self_id: &'static str) {
-        self.current_term = self.current_term + 1;
-        self.voted_for = Some(self_id);
+        self.element.current_term = self.element.current_term + 1;
+        self.element.voted_for = Some(self_id);
     }
 
     // Dummy implementation.
@@ -54,13 +58,13 @@ impl PersistentState {
     //   current_term : term number after update
     //   ok : true if the given term was not outdated
     pub fn update_term(&mut self, new_term: u64) -> (u64, bool) {
-        if new_term < self.current_term {
-            (self.current_term, false)
+        if new_term < self.element.current_term {
+            (self.element.current_term, false)
         } else {
             // Warning : this two updates must be committed simultaneously
-            self.current_term = new_term;
-            self.voted_for = None;
-            (self.current_term, true)
+            self.element.current_term = new_term;
+            self.element.voted_for = None;
+            (self.element.current_term, true)
         }
     }
 
@@ -68,9 +72,9 @@ impl PersistentState {
     // return ok
     //   ok : true if candidate received a vote
     pub fn try_vote(&mut self, candidate: &'static str) -> bool {
-        match &self.voted_for {
+        match &self.element.voted_for {
             None => {
-                self.voted_for = Some(candidate);
+                self.element.voted_for = Some(candidate);
                 true
             }
             Some(recipient) => *recipient == candidate,
