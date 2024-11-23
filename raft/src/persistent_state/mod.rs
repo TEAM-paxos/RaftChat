f5685e4c999f823c4bf641ce37df261d7b9a8d8b
damhiya <damhiya@gmail.com>
2024-11-23 14:50:39 +0900

    implement election restriction

--- raft/src/persistent_state/mod.rs
+++ raft/src/persistent_state/mod.rs
@@ -36,7 +36,6 @@ impl PersistentState {
     // return (current_term, ok)
     //   current_term : term number after update
     //   ok : true if the given term was not outdated
-    // return true  if updated
     pub fn update_term(&mut self, new_term: u64) -> (u64, bool) {
         if new_term < self.current_term {
             (self.current_term, false)
@@ -49,24 +48,15 @@ impl PersistentState {
     }
 
     // Dummy implementation
-    // return (current_term, ok)
-    //   current_term : term number after update
+    // return ok
     //   ok : true if candidate received a vote
-    pub fn try_vote(&mut self, new_term: u64, candidate: &'static str) -> (u64, bool) {
-        if new_term < self.current_term {
-            (self.current_term, false)
-        } else if self.current_term < new_term {
-            self.current_term = new_term;
-            self.voted_for = Some(candidate);
-            (self.current_term, true)
-        } else {
-            match &self.voted_for {
-                None => {
-                    self.voted_for = Some(candidate);
-                    (self.current_term, true)
-                }
-                Some(recipient) => (self.current_term, *recipient == candidate),
+    pub fn try_vote(&mut self, candidate: &'static str) -> bool {
+        match &self.voted_for {
+            None => {
+                self.voted_for = Some(candidate);
+                true
             }
+            Some(recipient) => *recipient == candidate,
         }
     }
 }
