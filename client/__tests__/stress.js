0b70eb5a3dff7de913d6e6b6e0d454ed5dfc909b
high-ubuntu <highcloud100@inha.edu>
2024-12-13 00:04:03 +0900

    Add stress test code

--- client/__tests__/stress.js
+++ client/__tests__/stress.js
@@ -2,16 +2,37 @@ import fetch from 'node-fetch'
 import { Engine } from '../static/engine.js'
 import { Storage } from '../static/storage.js';
 
-let engine = new Engine(new Storage(), false);
+
 
 fetch("http://127.0.0.1:3000/get_info")
     .then((response) => response.json())
     .then((data) => {
     console.log(data);
     
-    engine.setup(data, () => {
-        console.log("-----------------------------");
-        engine.sendToServer("hi");
+    const tasks = [];
+    const engines = []; 
+    
+    console.log = function() {};
+
+    for (let i = 0; i < 300; i++) {
+        engines.push(new Engine(new Storage(), false));
+        
+        tasks.push(
+            new Promise((resolve, reject) => {
+                engines[i].setup(data, () => {
+                    engines[i].sendToServer(`hi ${i + 1}`);
+                    resolve();
+                });
+            })
+        );
+    }
+
+    Promise.all(tasks).then(() => {
+        console.log("All tasks are complete.");
     });
+
+    setTimeout(() => { // 15000ms = 15초
+        process.exit();
+      }, 300000);  
 });
 
